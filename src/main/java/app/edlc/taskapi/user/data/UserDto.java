400544e5e183b4d6fad8d7489ce3493f497c41e9
edlcorrea <playered.lc@gmail.com>
2024-10-08 22:24:14 -0300

    Add User (entity) and UserDto implementation.

---
+++ src/main/java/app/edlc/taskapi/user/data/UserDto.java
@@ -0,0 +1,68 @@
+package app.edlc.taskapi.user.data;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class UserDto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String username;
+	private String password;
+	
+	public UserDto() {}
+	
+	// Getters, Setters, Equals and HashCode
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, password, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserDto other = (UserDto) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name)
+				&& Objects.equals(password, other.password) && Objects.equals(username, other.username);
+	}
+}
