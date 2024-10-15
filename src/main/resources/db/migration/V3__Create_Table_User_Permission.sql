0c87650737b54a1091aa800066d8e4591e8f2339
edlcorrea <playered.lc@gmail.com>
2024-10-15 11:19:30 -0300

    Fix migration of table user_permission to delete on cascade. Fix SQLite connection to allow foreign_keys.

--- src/main/resources/db/migration/V3__Create_Table_User_Permission.sql
+++ src/main/resources/db/migration/V3__Create_Table_User_Permission.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS user_permission (
   id_user INTEGER NOT NULL,
   id_permission INTEGER NOT NULL,
   PRIMARY KEY (id_user, id_permission),
-  FOREIGN KEY (id_user) REFERENCES user (id),
-  FOREIGN KEY (id_permission) REFERENCES permission (id)
+  FOREIGN KEY (id_user) REFERENCES user (id) ON DELETE CASCADE,
+  FOREIGN KEY (id_permission) REFERENCES permission (id) ON DELETE CASCADE
 );
